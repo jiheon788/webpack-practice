2f0b67f3a6c3106ff675297a2c92c71f6260733f
jiheon788 <jiheon788@ajou.ac.kr>
2023-01-02 17:04:49 +0900

    feat: 웹팩맛보기

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// `webpack` command will pick up this config setup by default
+var path = require('path');
+
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
